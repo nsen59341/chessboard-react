d08c2c0a7311da4309eb872acd39da97d82dfbd3
Natashasen <nsen59341@gmail.com>
2024-04-02 10:16:09 +0530

    added redux chess board, initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const rendeSqr = () => {
+    let sqrs = [];
+    for (let row = 0; row < 8; row++) {
+      const rowSquares = [];
+      for (let col = 0; col < 8; col++) {
+        const squareColor = (row + col) % 2 === 0 ? 'white' : 'black'; // Alternate colors
+        rowSquares.push(
+          <td key={`${row}-${col}`} id={`${row}-${col}`} className={`box ${squareColor}`} onMouseOver={(e) => showPath(e)}></td>
+        );
+      }
+      sqrs.push(<tr key={row}>{rowSquares}</tr>);
+    }
+    return sqrs;
+  }
+
+  const showPath = (e) => {
+    // console.log(e.target.id);
+    let id = e.target.id;
+
+    if(id===undefined){
+      return;
+    }
+    
+    removeYellow(e.target);
+
+    let [curr_row, curr_col] = id.split("-").map(idx => idx);
+
+    let directionVector = getDirectionVector('queen');
+    let maxRadius = getMaxiumRadius('queen');
+    let storageOfPossibleMoves = possibleMoves(parseInt(curr_row), parseInt(curr_col), 8, 8, directionVector, maxRadius);
+    colorMyPossibleMoves(storageOfPossibleMoves, e.target);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table id='table' className='table'>
+        <tbody>
+          {rendeSqr()}
+        </tbody>
+      </table>
     </div>
   );
+}
+
+function removeYellow(box){
+    box.classList.remove("yellow");
+}
+
+function getDirectionVector(player) {
+  if (player === "king") {
+      return [[1, 1], [-1, 1], [1, -1], [-1, -1], [0, 1], [1, 0], [0, -1], [-1, 0]];
+  } else if (player === "bishop") {
+      return [[1, 1], [-1, 1], [1, -1], [-1, -1]];
+  } else if (player === "queen") {
+      return [[1, 1], [-1, 1], [1, -1], [-1, -1], [0, 1], [1, 0], [0, -1], [-1, 0]]
+  } else if (player === "knight") {
+      return [[-2, -1], [-1, -2], [1, -2], [2, -1], [-2, 1], [-1, 2], [1, 2], [2, 1]];
+  } else if (player === "rook") {
+      return [[-1,0],[1,0],[0,-1],[0,1]];
+  }
+}
+
+function getMaxiumRadius(player) {
+  if (player === 'king') {
+      return 1;
+  } else if (player === "bishop") {
+      return 7;
+  } else if (player === "queen") {
+      return 7;
+  } else if (player === "knight") {
+      return 1;
+  } else if (player === "rook") {
+      return 7
+  }
+}
+function possibleMoves(curr_row, curr_col, N, M, direction, maxRadius) {
+  let storageOfPossibleMoves = {};
+  for (let dir of direction) {
+      for (let radius = 0; radius <= maxRadius; radius++) {
+          let r = curr_row + (radius * dir[0]);
+          let c = curr_col + (radius * dir[1]);
+
+          if (r >= 0 && c >= 0 && r < N && c < M) {
+              let dataIndex = `${r}-${c}`;
+              storageOfPossibleMoves[dataIndex] = true;
+          } else {
+              break;
+          }
+      }
+  }
+  
+  return storageOfPossibleMoves;
+}
+
+function colorMyPossibleMoves(storageOfPossibleMoves, box) {
+    console.log(box); 
+    let curr_dataIndex = box.id;
+    if (storageOfPossibleMoves[curr_dataIndex]) {
+      box.classList.add("yellow");
+    }
 }
 
 export default App;
